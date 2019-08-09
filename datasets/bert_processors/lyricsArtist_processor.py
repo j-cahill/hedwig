b286bc609073df593c082552a946b7b6d242712b
Jesse <jcahill225@gmail.com>
2019-08-09 08:44:05 -0400

    added separate LyricsGenre and LyricsArtist dataset classes

---
+++ datasets/bert_processors/lyricsArtist_processor.py
@@ -0,0 +1,41 @@
+import os
+
+from datasets.bert_processors.abstract_processor import BertProcessor, InputExample
+
+
+class LyricsArtistProcessor(BertProcessor):
+    def __init__(self):
+        self.NAME = 'LyricsArtist'
+
+    def set_num_classes_(self, data_dir):
+        with open(os.path.join(data_dir, 'LyricsArtist', 'train.tsv'), 'r') as f:
+            l1 = f.readline().split('\t')
+
+        # from one-hot class vector
+        self.NUM_CLASSES = len(l1[0])
+        self.IS_MULTILABEL = self.NUM_CLASSES > 2
+
+    def get_train_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, 'LyricsArtist', 'train.tsv')), 'train')
+
+    def get_dev_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, 'LyricsArtist', 'dev.tsv')), 'dev')
+
+    def get_test_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, 'LyricsArtist', 'test.tsv')), 'test')
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[1]
+            label = line[0]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
